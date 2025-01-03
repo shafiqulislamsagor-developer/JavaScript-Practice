3eee1caf95fc73acebaff8fc5e3988003a793ccc
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-01-03 23:35:10 +0600

    javascript practice all setup

---
+++ index.js
@@ -0,0 +1,6 @@
+const text = document.getElementById("demo");
+const button = document.getElementById("btn");
+
+button.addEventListener("click", () => {
+  text.innerHTML = "hello javascript world!";
+});
