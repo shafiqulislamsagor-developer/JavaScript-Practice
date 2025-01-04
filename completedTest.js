03b680ea7b540940d0b0cdbaf0de618b95681af6
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-01-04 15:58:34 +0600

    Data types , operators learn

---
+++ completedTest.js
@@ -0,0 +1,38 @@
+// completed practice this code store the current file
+
+// carNa = "car";
+
+// var carNa;
+
+let x = ["x", "y", "z"];
+
+let y = 9;
+
+let z = 10;
+
+try {
+  if (x) {
+    //   if ((x |= y)) {
+    //   if ((x ^= y)) {
+    //   if ((x &= y)) {
+    // x += y;
+    // x -= y;
+    // x *= y;
+    // x /= y;
+    // x %= y;
+    // x **= y;
+    // x <<= y;
+    // x >>= y;
+    // x >>>= y;
+    // console.log(12.0);
+    // console.log(12);
+    // console.log(1e-5);
+    // console.log(BigInt("1234567890"));
+    console.log("thik ase code");
+    console.log(x[1]);
+  } else {
+    console.log("thik nai");
+  }
+} catch {
+  console.log("error");
+}
